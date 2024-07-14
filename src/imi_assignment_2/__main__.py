ab87e99f61b7c29c10acb806d3a2d2bd7b824327
mitukou1109 <mitukou1109@gmail.com>
2024-07-14 12:09:17 +0900

    change config

--- src/imi_assignment_2/__main__.py
+++ src/imi_assignment_2/__main__.py
@@ -8,14 +8,14 @@ iris = sklearn.datasets.load_iris()
 X = iris.data[iris.target != 0, 2:]
 y = iris.target[iris.target != 0] * 2 - 3
 
-adaboost = AdaBoost(num_classifiers=100)
+adaboost = AdaBoost(num_classifiers=10)
 adaboost.fit(X, y)
 
 x_1_min, x_1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 x_2_min, x_2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 X1, X2 = np.meshgrid(np.arange(x_1_min, x_1_max, 0.1), np.arange(x_2_min, x_2_max, 0.1))
-y_mesh = adaboost.predict(np.column_stack((X1.ravel(), X2.ravel())))
+Z = adaboost.predict(np.column_stack((X1.ravel(), X2.ravel()))).reshape(X1.shape)
 
-plt.contourf(X1, X2, y_mesh.reshape(X1.shape), alpha=0.5)
+plt.contourf(X1, X2, Z, alpha=0.5, levels=1)
 plt.scatter(X[:, 0], X[:, 1], c=y)
 plt.show()
