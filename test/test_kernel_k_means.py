8bd2a8e293aaa70c19e5e8bcde50a02afe08c3cb
mitukou1109 <mitukou1109@gmail.com>
2024-07-14 20:44:34 +0900

    swap class labels for visualization

--- test/test_kernel_k_means.py
+++ test/test_kernel_k_means.py
@@ -10,9 +10,13 @@ if __name__ == "__main__":
 
     linear_k_means = KernelKMeans(LinearKernel())
     linear_k_means_labels = linear_k_means.classify(X, K=2)
+    if np.count_nonzero(linear_k_means_labels != y) > y.shape[0] // 2:
+        linear_k_means_labels = np.where(linear_k_means_labels == 0, 1, 0)
 
-    gauss_k_means = KernelKMeans(GaussKernel(gamma=5e1))
-    gauss_k_means_labels = gauss_k_means.classify(X, K=2)
+    gauss_k_means = KernelKMeans(GaussKernel(gamma=2.5))
+    gauss_k_means_labels = gauss_k_means.classify(X, K=2, converge_threshold=50)
+    if np.count_nonzero(gauss_k_means_labels != y) > y.shape[0] // 2:
+        gauss_k_means_labels = np.where(gauss_k_means_labels == 0, 1, 0)
 
     fig, axs = plt.subplots(1, 3, figsize=(14, 4))
     axs[0].scatter(X[:, 0], X[:, 1], c=y, cmap="bwr")
