4a4269ae2b12d8492f32395b73e0cc5339b143d4
mitukou1109 <mitukou1109@gmail.com>
2024-07-14 12:41:31 +0900

    tolerate classifier if error rate is not 0.5

--- src/imi_assignment_2/decision_stump.py
+++ src/imi_assignment_2/decision_stump.py
@@ -7,37 +7,33 @@ class DecisionStump:
         self.index: int = None
         self.threshold: float = None
         self.reliability: float = None
+        self.rng = np.random.default_rng()
+
+    def _predict(
+        self, X: np.ndarray, sign: int, index: int, threshold: float
+    ) -> np.ndarray:
+        return sign * (
+            2 * ((X[:, index] - threshold) > 0) - 1
+        )  # Not using np.sign to prevent zero predictions
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        return self.sign * (
-            2 * ((X[:, self.index] - self.threshold) > 0) - 1
-        )  # Prevent zero predictions
+        return self._predict(X, self.sign, self.index, self.threshold)
 
     def fit(self, X: np.ndarray, y: np.ndarray, weight: np.ndarray) -> None:
-        num_samples, num_features = X.shape
-        min_error = np.inf
-        for i in range(num_features):
-            row_ind_sorted = np.argsort(X[:, i])
-            labels_sorted = y[row_ind_sorted]
-            weights_sorted = weight[row_ind_sorted]
-            for j in range(1, num_samples):
-                threshold = (X[row_ind_sorted[j - 1], i] + X[row_ind_sorted[j], i]) / 2
-                error = np.dot(
-                    weights_sorted,
-                    (np.sign(X[:, i] - threshold).astype(int) != labels_sorted).astype(
-                        int
-                    ),
-                )
-                error_inv = np.dot(
-                    weights_sorted,
-                    (-np.sign(X[:, i] - threshold).astype(int) != labels_sorted).astype(
-                        int
-                    ),
-                )
-                if error < min_error or error_inv < min_error:
-                    min_error = min(error, error_inv)
-                    error_rate = min_error / np.sum(weights_sorted)
-                    self.sign = 1 if error < error_inv else -1
-                    self.index = i
+        indices = np.arange(X.shape[1])
+        self.rng.shuffle(indices)
+        for index in indices:
+            values = np.unique(X[:, index])
+            self.rng.shuffle(values)
+            for threshold in values:
+                error_rate = np.dot(
+                    weight, (self._predict(X, 1, index, threshold) != y).astype(int)
+                ) / np.sum(weight)
+                if error_rate == 0.5 or error_rate <= 0 or error_rate >= 1:
+                    continue
+                else:
+                    self.sign = 1 if error_rate < 0.5 else -1
+                    self.index = index
                     self.threshold = threshold
-                    self.reliability = np.log((1 - error_rate) / error_rate)
+                    self.reliability = np.log(1 / error_rate - 1)
+                    break
